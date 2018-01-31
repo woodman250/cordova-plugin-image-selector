1780305aa068d3ffab58a2a89e5a288cf74c73a0
ironskinspirit <dds103@163.com>
2018-01-31 18:39:31 +0800

    add ios support

--- www/MediaPicker.js
+++ www/MediaPicker.js
@@ -13,7 +13,14 @@ var MediaPicker = {
 	},
 	extractThumbnail:function(arg0, success, error) {
     	exec(success, error, "MediaPicker", "extractThumbnail", [arg0]);
-	}
+	}，
+	getOStype:function() {
+		var u = navigator.userAgent; 
+		var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端 
+		var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+		
+	}	
+	
 };
 
 module.exports = MediaPicker;
