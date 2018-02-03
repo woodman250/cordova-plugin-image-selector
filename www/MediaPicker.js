599c46189c82486436476c71774fdad80ecc719e
ironskinspirit <dds103@613.com>
2018-02-03 19:30:20 +0800

    android done

--- www/MediaPicker.js
+++ www/MediaPicker.js
@@ -3,7 +3,25 @@ var exec = require('cordova/exec');
 
 var MediaPicker = {
     getMedias:function(arg0, success, error) {
-    	exec(success, error, "MediaPicker", "getMedias", [arg0]);
+		alert(navigator.userAgent);
+		if(navigator.userAgent.match(/Android/i)){
+			arg0.selectMode = 100;
+			arg0.showThumbnail = true;
+			exec(success, error, "MediaPicker", "getMedias", [arg0]);
+		}else if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){
+			if (!arg0) {
+				arg0 = {};
+			}
+	
+			var params = {
+				maximumImagesCount: arg0.maxSelectCount ? arg0.maxSelectCount : 9,
+				width: arg0.width ? arg0.width : 1024,
+				height: arg0.height ? arg0.height : 1024,
+				quality: arg0.quality ? arg0.quality : 80
+			};
+
+			cordova.exec(success, fail, "ImagePicker", "getPictures", [params]);
+    	}
 	},
 	photoLibrary:function(arg0, success, error) {
     	exec(success, error, "MediaPicker", "photoLibrary", [arg0]);
@@ -13,13 +31,7 @@ var MediaPicker = {
 	},
 	extractThumbnail:function(arg0, success, error) {
     	exec(success, error, "MediaPicker", "extractThumbnail", [arg0]);
-	}，
-	getOStype:function() {
-		var u = navigator.userAgent; 
-		var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端 
-		var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-		
-	}	
+	}
 	
 };
 
